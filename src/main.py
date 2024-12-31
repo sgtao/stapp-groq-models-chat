9fb9c7b19c4d3447eb376a77b5d2680fbd6be2b9
Shogo Ogami <sg.tao.so@gmail.com>
2024-12-31 15:01:24 +0900

    Fix:update main page for rename page title

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Ex-stream-ly Cool App",
+    page_title="Groq-API App",
     page_icon="🏠",
     layout="wide",
     initial_sidebar_state="expanded",
